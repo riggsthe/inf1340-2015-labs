b408e4353e7ae20db1f63063ff5143ed8ffd37f9
riggsthe <tessie.riggs@gmail.com>
2015-10-01 12:58:13 -0400

    Lab exercise finished (for real this time!)

--- lab1.py
+++ lab1.py
@@ -8,8 +8,8 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-
-"""
+def vowels_and_consonants():
+    """
     Exercise: Vowel or Consonant
     Reads a letter of the alphabet from the user. (You can assume that it's
     lowercase.) If the user enters a, e, i, o or u then your program should
@@ -18,24 +18,27 @@ __license__ = "MIT License"
     display a message indicating that the letter is a "consonant".
     """
 # Issue instruction to user to input a letter
-letter = raw_input("input a letter")
+    letter = raw_input("input a letter")
 # Establish vowels with if statements
-if letter == "a":
-    print("vowel")
-elif letter == "e":
-    print("vowel")
-elif letter == "i":
-    print("vowel")
-elif letter == "o":
-    print("vowel")
-elif letter == "u":
-    print("vowel")
+    if letter == "a":
+        print("vowel")
+    elif letter == "e":
+        print("vowel")
+    elif letter == "i":
+        print("vowel")
+    elif letter == "o":
+        print("vowel")
+    elif letter == "u":
+        print("vowel")
 
 # Establish y is a vowel sometimes, sometimes a consonant with another if statement
-elif letter == "y":
+    elif letter == "y":
         print("Sometimes a vowel, sometimes a consonant")
 # Establish rules for consonants with else statement
 
-else:
-    print("consonant")
+    else:
+        print("consonant")
 # For some reason when a vowel is entered (besides y) it prints both "vowel" and "consonant". how to fix??
+
+vowels_and_consonants()
+
